5444b4f1d8e2d7ad6288fdcc81f9dd09db1b2cd4
Unknown <bworth227@gmail.com>
2018-01-22 08:42:59 -0700

    First version of assignment 1

---
+++ Assignment1/Assignment 1/interview.js
@@ -0,0 +1,8 @@
+var first_name;
+var last_name;
+var color;
+
+first_name = prompt("What is your first name?");
+last_name = prompt("What is your last name?");
+color = prompt("What is your favorite color?")
+alert("Your name is " + first_name + " " + last_name + " and your favorite color is " + color + ".");
